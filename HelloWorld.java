1b671a927efa3646afcc64c0ea3588e048fb72f5
sushmanjnv13@gmail.com <sushmanjnv13@gmail.com>
2020-09-16 11:35:28 +0000

    new Hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,19 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! Citi is our company");
-    }
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
+
+
+
+   
+
 
 
 
